ce50d7bcfdbc9a1f1b8742d107fa1bc7f05981d7
ilnuriskander <132539705+ilnuriskander@users.noreply.github.com>
2023-06-06 10:43:04 +0500

    9.2

--- lesson 9/lesson9_task2.py
+++ lesson 9/lesson9_task2.py
@@ -25,20 +25,19 @@
 
 import datetime
 
-
-
+#file_log='log.txt'
 dt = datetime.datetime.now()
 
 def func_log(func, file_log='log.txt'):
     def wrapper():
         logs = open(file_log, 'a+', encoding='utf-8')
-        logs.write(func())
+        func_info = func.__name__, 'вызвана', dt.strftime('%d.%m %H:%M:%S'), '\n'
+        func_info = ' '.join(func_info)
+        logs.write(func_info)
         logs.close()
+        return func
+    return wrapper()
 
 @func_log
-def f():
-    a = f.__name__, 'вызвана', dt.strftime('%d.%m %H:%M:%S')
-    b = ' '.join(a)
-    return b
-
-# Здесь пишем код
+def func():
+    print('запуск функции')
